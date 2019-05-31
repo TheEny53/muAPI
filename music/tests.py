15d2b896e5ae46fcc11aaad0347424c6460186da
Enzo Hilzinger <enzo.hilzinger@sap.com>
2019-05-31 13:56:14 +0200

    Added tests for detail views

--- music/tests.py
+++ music/tests.py
@@ -15,16 +15,16 @@ class BaseViewTest(APITestCase):
                       rating="", wiki_link="", picture_link=""):
         if (name != "" and founding_year != "" and founding_country != ""
                 and rating != "" and is_active != "" and wiki_link != ""):
-            return Artist.objects.create(name=name, founding_year=founding_year, 
+            return Artist.objects.create(name=name, founding_year=founding_year,
                                          founding_country=founding_country, is_active=is_active,
-                                         rating=rating, wiki_link=wiki_link, 
+                                         rating=rating, wiki_link=wiki_link,
                                          picture_link=picture_link)
 
     @staticmethod
     def create_genre(name="", country_of_origin="", year_of_establishment="", wiki_link=""):
         if name != "" and country_of_origin != "" and wiki_link != "":
             return Genre.objects.create(name=name, country_of_origin=country_of_origin,
-                                        year_of_establishment=year_of_establishment, 
+                                        year_of_establishment=year_of_establishment,
                                         wiki_link=wiki_link)
 
     @staticmethod
@@ -32,23 +32,58 @@ class BaseViewTest(APITestCase):
                      rating="", label="", wiki_link="", picture_link=""):
         if name != "" and artist != "" and rating != "" and wiki_link != "":
             return Album.objects.create(name=name, release_date=release_date, artist=artist,
-            length=length, produced_at=produced_at, producer=producer, rating=rating,
-            label=label, wiki_link=wiki_link, picture_link=picture_link)
+                                        length=length, produced_at=produced_at, producer=producer, rating=rating,
+                                        label=label, wiki_link=wiki_link, picture_link=picture_link)
 
     @staticmethod
     def create_song(name="", release_date="",  artist="", length="",
-                     rating="", wiki_link="", picture_link="", genre=""):
+                    rating="", wiki_link="", picture_link="", genre=""):
         if name != "" and artist != "" and rating != "" and wiki_link != "":
             return Song.objects.create(name=name, release_date=release_date,
                                        artist=artist, length=length,
                                        rating=rating, wiki_link=wiki_link,
                                        picture_link=picture_link, genre=genre)
 
+    def fetch_a_song(self, pk=0):
+        return self.client.get(
+            reverse(
+                "song-detail",
+                kwargs={
+                    "version": "v1",
+                    "pk": pk
+                }
+            )
+        )
+
+    def fetch_an_album(self, pk=0):
+        return self.client.get(
+            reverse(
+                "album-detail",
+                kwargs={
+                    "version": "v1",
+                    "pk": pk
+                }
+            )
+        )
+
+    def fetch_single(self, pk=0, revUrl=""):
+        if revUrl != "":
+            return self.client.get(
+                reverse(
+                    revUrl,
+                    kwargs={
+                        "version": "v1",
+                        "pk": pk
+                    }
+                )
+            )
+
     def setUp(self):
+        # create test data
         # 1. Artists
-        dummy_artist = self.create_artist("Sean Paul", "1987", "JA", "True", "3", 
+        dummy_artist = self.create_artist("Sean Paul", "1987", "JA", "True", "3",
                                           "wiki.com", "wiki.com")
-        self.create_artist("Michael Jackson", "1976", "US", "False", "4", 
+        self.create_artist("Michael Jackson", "1976", "US", "False", "4",
                            "wiki.com/us", "wiki.com/pic")
 
         # 2. Genres
@@ -62,9 +97,16 @@ class BaseViewTest(APITestCase):
 
         # 4. Songs
         dummy_song = self.create_song("The man comes around", "2002-12-01",
-                         dummy_artist, "4.53", "4", "w.com", "p.com", dummy_genre)
+                                      dummy_artist, "4.53", "4", "w.com", "p.com", dummy_genre)
         # add album manually as it is a many-to-many relation
         dummy_song.album.add(dummy_album)
+
+        self.valid_song_id = 1
+        self.valid_album_id = 1
+        self.valid_artist_id = 1
+        self.valid_genre_id = 1
+
+        self.invalid_id = 100
 
 
 class GetAllTest(BaseViewTest):
@@ -89,27 +131,26 @@ class GetAllTest(BaseViewTest):
         This test ensures that all genres added in the setUp method
         exist when we make a GET request to the genres/ endpoint
         """
-         # hit the API endpoint
+        # hit the API endpoint
         response = self.client.get(
             reverse("genres-all", kwargs={"version": "v1"})
         )
-         # fetch the data from db
+        # fetch the data from db
         expected = Genre.objects.all()
         serialized = GenreSerializer(expected, many=True)
         self.assertEqual(response.data, serialized.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    
     def test_get_all_albums(self):
         """
         This test ensures that all albums added in the setUp method
         exist when we make a GET request to the albums/ endpoint
         """
-         # hit the API endpoint
+        # hit the API endpoint
         response = self.client.get(
             reverse("albums-all", kwargs={"version": "v1"})
         )
-         # fetch the data from db
+        # fetch the data from db
         expected = Album.objects.all()
         serialized = AlbumSerializer(expected, many=True)
         self.assertEqual(response.data, serialized.data)
@@ -120,12 +161,70 @@ class GetAllTest(BaseViewTest):
         This test ensures that all albums added in the setUp method
         exist when we make a GET request to the albums/ endpoint
         """
-         # hit the API endpoint
+        # hit the API endpoint
         response = self.client.get(
             reverse("songs-all", kwargs={"version": "v1"})
         )
-         # fetch the data from db
+        # fetch the data from db
         expected = Song.objects.all()
         serialized = SongSerializer(expected, many=True)
         self.assertEqual(response.data, serialized.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class GetSingleTest(BaseViewTest):
+    def test_get_single_album(self):
+        response = self.fetch_single(self.valid_album_id, "album-detail")
+        expected = Album.objects.get(pk=self.valid_album_id)
+        serialized = AlbumSerializer(expected)
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.fetch_single(self.invalid_id, "album-detail")
+        self.assertEqual(
+            response.data["message"],
+            "Album with id: {} does not exist".format(self.invalid_id)
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_single_song(self):
+        response = self.fetch_single(self.valid_song_id, "song-detail")
+        expected = Song.objects.get(pk=self.valid_song_id)
+        serialized = SongSerializer(expected)
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.fetch_single(self.invalid_id, "song-detail")
+        self.assertEqual(
+            response.data["message"],
+            "Song with id: {} does not exist".format(self.invalid_id)
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_single_artist(self):
+        response = self.fetch_single(self.valid_artist_id, "artist-detail")
+        expected = Artist.objects.get(pk=self.valid_artist_id)
+        serialized = ArtistSerializer(expected)
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.fetch_single(self.invalid_id, "artist-detail")
+        self.assertEqual(
+            response.data["message"],
+            "Artist with id: {} does not exist".format(self.invalid_id)
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_single_genre(self):
+        response = self.fetch_single(self.valid_genre_id, "genre-detail")
+        expected = Genre.objects.get(pk=self.valid_artist_id)
+        serialized = GenreSerializer(expected)
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.fetch_single(self.invalid_id, "genre-detail")
+        self.assertEqual(
+            response.data["message"],
+            "Genre with id: {} does not exist".format(self.invalid_id)
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
