e2fc793d9842ab22edfe3fc203d19d8d2a53415a
Enzo Hilzinger <enzo.hilzinger@sap.com>
2019-05-31 15:07:05 +0200

    removed snippets app

--- restapi/settings.py
+++ restapi/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'music',
-    'rest_framework',
-    'snippets'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
