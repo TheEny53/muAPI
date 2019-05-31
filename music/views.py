b493bde0d6f4b49a6808a316c20378c79c4a0076
Enzo Hilzinger <enzo.hilzinger@sap.com>
2019-05-31 15:40:07 +0200

    edited import

--- music/views.py
+++ music/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import status
 from rest_framework import generics
 from rest_framework.response import Response
 from .models import *
-from .serializers import *
+from .serializers import SongSerializer, AlbumSerializer, GenreSerializer, ArtistSerializer
 
 
 class ListSongsView(generics.ListAPIView):
