1de2774e17cfcb90afbe83868df9f70ac5fbd58e
Enzo Hilzinger <enzo.hilzinger@sap.com>
2019-06-04 12:38:31 +0200

    modified admin views

--- music/admin.py
+++ music/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 from .models import Song, Album, Artist, Genre
 
-admin.site.register(Song)
-admin.site.register(Album)
-admin.site.register(Artist)
-admin.site.register(Genre)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('name', 'artist', 'rating', 'release_date', 'genre')
+    list_filter = ('rating',)
+    search_fields = ['name', 'artist']
+    filter_horizontal = ('album',)
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('name', 'artist', 'rating', 'release_date')
+    list_filter = ('rating',)
+    search_fields = ['name', 'artist']
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'founding_country', 'rating', 'is_active')
+    list_filter = ('rating', 'founding_country', 'is_active')
+    search_fields = ('name', 'founding_country')
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country_of_origin', 'year_of_establishment')
+    list_filter = ('country_of_origin', 'year_of_establishment')
+    search_fields = ('name', 'country_of_origin', 'year_of_establishment')
+
+admin.site.register(Song, SongAdmin)
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Genre, GenreAdmin)
--- music/views.py
+++ music/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import status
 from rest_framework import generics
+from django.core.validators import validate_email
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from rest_framework_jwt.settings import api_settings
 from rest_framework import permissions
@@ -22,17 +24,18 @@ class RegisterUsersView(generics.CreateAPIView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request, *args, **kwargs):
-        username = request.data.get("username", "")
-        password = request.data.get("password", "")
-        email = request.data.get("email", "")
-        if not username and not password and not email:
+        if request.data.get("username", "") and request.data.get("password", "") and request.data.get("email", ""):
+            username = request.data.get("username", "")
+            password = request.data.get("password", "")
+            email = request.data.get("email", "")
+        else:
             return Response(
                 data={
                     "message": "username, password and email is required to register a user"
                 },
                 status=status.HTTP_400_BAD_REQUEST
             )
-        new_user = User.objects.create_user(
+        User.objects.create_user(
             username=username, password=password, email=email
         )
         return Response(status=status.HTTP_201_CREATED)
@@ -61,9 +64,9 @@ class LoginView(generics.CreateAPIView):
                     jwt_payload_handler(user)
                 )})
             serializer.is_valid()
-            return Response(serializer.data)
+            r = Response(serializer.data)
+            return r
         return Response(status=status.HTTP_401_UNAUTHORIZED)
-
 
 class ListSongsView(generics.ListAPIView):
     """
@@ -281,6 +284,7 @@ class DetailArtistView(generics.RetrieveAPIView):
     # View is non-editable
     """
     GET artists/:id/
+    DELETE artists/:id/
     """
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
@@ -325,6 +329,7 @@ class DetailArtistView(generics.RetrieveAPIView):
 class DetailGenreView(generics.RetrieveAPIView):
     """
     GET genres/:id/
+    DELETE genres/:id/
     """
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
