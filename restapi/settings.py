a71cfd540e1d372d90400844150fb762437f11cb
Enzo Hilzinger <enzo.hilzinger@sap.com>
2019-07-16 16:11:03 +0200

    refix

--- restapi/settings.py
+++ restapi/settings.py
@@ -169,7 +169,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'dist')
 STATIC_URL = '/dist/'
 
 # Extra places for collectstatic to find static files.
